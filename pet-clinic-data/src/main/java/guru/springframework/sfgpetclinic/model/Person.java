6b2f96fd3e40b673e5cb5f74dbdecabf4d34e10c
Andry Andrianambinina <andry.andrianambinina@proximitybbdo.fr>
2021-08-16 09:36:40 +0400

    Add relationships annotation

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
-import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
-@Entity
+@MappedSuperclass
 public class Person extends BaseEntity {
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
